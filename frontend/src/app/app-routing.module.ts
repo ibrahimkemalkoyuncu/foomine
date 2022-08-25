aa1d144988cc139e3007c408ba685a5229405410
ibrahimkemalkoyuncu <ibrahimkemalkoyuncu@gmail.com>
2022-08-25 16:07:18 +0300

    Create sidenav

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoupensComponent } from './page/coupens/coupens.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { MediaComponent } from './page/media/media.component';
+import { PagesComponent } from './page/pages/pages.component';
+import { ProductsComponent } from './page/products/products.component';
+import { SettingsComponent } from './page/settings/settings.component';
+import { StatisticsComponent } from './page/statistics/statistics.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'coupens', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { SidenavComponent } from './components/partials/sidenav/sidenav.component';
+import { BodyComponent } from './page/body/body.component';
+import { CoupensComponent } from './page/coupens/coupens.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { MediaComponent } from './page/media/media.component';
+import { PagesComponent } from './page/pages/pages.component';
+import { ProductsComponent } from './page/products/products.component';
+import { SettingsComponent } from './page/settings/settings.component';
+import { StatisticsComponent } from './page/statistics/statistics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    BodyComponent,
+    HeaderComponent,
+    SidenavComponent,
+    DashboardComponent,
+    ProductsComponent,
+    StatisticsComponent,
+    CoupensComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
